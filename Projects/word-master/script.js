5aa3293d5ed9406e36a3085abc723a5cd79fe0a0
SirajAtFiverr <sirajatfiverr@gmail.com>
2023-02-26 10:48:25 +0500

    fixing bugs

--- Projects/word-master/script.js
+++ Projects/word-master/script.js
@@ -1,7 +1,7 @@
-let blocks = Object.values(document.getElementsByClassName("letter-block"));
-
 const GET = "https://words.dev-apis.com/word-of-the-day"
 const POST = "https://words.dev-apis.com/validate-word"
+
+let blocks = Object.values(document.getElementsByClassName("letter-block"));
 
 function showSpinner() {
     document.getElementsByClassName('spinner')[0].style.visibility = 'visible';
@@ -74,21 +74,23 @@ async function main(){
     // if key any key is pressed
     function (event){
 
-
         // key pressed is a letter (A-Z)
         if(isLetter(event.key)){
             if(block_index == 5) block_index--;
             blocks[block_index].innerText = event.key.toUpperCase();
             if(block_index < 5) block_index ++;
-            return; 
+            return;
         }
 
         // if backspace key is pressed
         if(event.key === "Backspace"){
-            // deleting text of block
-            blocks[block_index-1].innerText = "";
-            // decreasing block index by one, if index is greater than 0
-            if(block_index > 0) block_index --;
+            // if index is greater than 0
+            if(block_index > 0){
+                // decreasing block index by one
+                block_index --;
+                // deleting text of block
+                blocks[block_index].innerText = "";
+            }
             return;
         }
 
@@ -96,6 +98,8 @@ async function main(){
         if(event.key === "Enter"){
             // getting word of row
             let word = getWord(block_index);
+            // if word is incompete, go out without doing anything
+            if(word.length != 5) return;
             // check if it is a valid word by sending a POST request
             validateWord(word).then(function (response){
                 // if word is not valid, go out with performing some animation
@@ -108,24 +112,46 @@ async function main(){
                 if(word === processedResponse.word.toUpperCase()){
                     alert("You Have Won!!!!");
                     performWinningAnimation(block_index);
+                    block_index = 0;
                     return;
                 }
 
                 // if word is not mactched, 
                 // check for each character of word wether it is in secret word or not
                 for(let i=0; i<5; i++){
-                    // if secret word has ith index cahracter
-                    if(processedResponse.word.toUpperCase().includes(word[i]))
-                        // add class "not-include" in ith element to change it's color using css 
+
+                    // if secret word has ith character at correct position
+                    if(processedResponse.word.toUpperCase().indexOf(word[i]) == i){
+                        // add class "correct-pos" in ith element to change it's color to green using css
+                        blocks[i].classList.add("correct-pos");
+                        continue;
+                    }
+
+                    console.log(word.substr(i));
+                    console.log(word.substr(i).includes(word[i]))
+
+                    // if secret word has not ith index cahracter
+                    if((!processedResponse.word.toUpperCase().includes(word[i])) || 
+                       word.substring(0, i).includes(word[i]))
+                    {
+                        // add class "not-include" in ith element to change it's color to gray using css
                         blocks[i].classList.add("not-include");
-                    else
-                        // if secret word has not isth character
-                        // add class "include" in ith element to change it's color using css
-                        blocks[i].classList.add("include");
+                        continue;
+                    }
+                        
+                    // if character is not at correct position
+                    // add class "include" in ith element to change it's color to yellow using css 
+                    blocks[i].classList.add("include");
+                    
                 }
+
                 block_index = 0;
                 removeRow(blocks);
-                
+                // if there if all rows completed
+                if(blocks.length == 0){
+                    // display popup message and secret word 
+                    alert("You Have Lost, The word was " + processedResponse.word.toUpperCase());
+                }
             });
             
             }
